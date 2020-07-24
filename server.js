eb227f5f844a6c3349c1c86a2271df04dda77e94
Evan Overman <evanrileyoverman@gmail.com>
2020-07-23 20:00:25 -0700

    bull

--- server.js
+++ server.js
@@ -15,4 +15,9 @@ server.get('/style.css', (request, response) => {
     response.sendFile(path.join(__dirname + '/style.css'));
 });
 
+server.get('/mc', (request, response) => {
+    console.log('[i] Server got request for /mc');
+    response.sendFile(path.join(__dirname + '/pages/mc.html'));
+});
+
 server.listen(3000, console.log('[i] Server listening on port 3000'))
