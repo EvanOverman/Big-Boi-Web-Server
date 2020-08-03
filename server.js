9f0ffa872400d8dd1a2780814982271c3c41c8c8
Evan Overman <evanrileyoverman@gmail.com>
2020-08-03 16:35:36 -0700

    scrapping some stuff

--- server.js
+++ server.js
@@ -2,60 +2,33 @@ const { exec } = require('child_process');
 const { request, ServerResponse } = require('https');
 const { response } = require('express');
 
-let server = require('express')();
+let vhost = require("vhost");
 let path = require('path');
+const express = require('express');
+let server = express();
+let www = express()
 
-// Home page.
-server.get('/', (request, response) => {
-    console.log('[i] Server got request for /...');
-    response.sendFile(path.join(__dirname + '/pages/Home.html'));
+www.use(
+    vhost("www.*", function handle(request, response, next) {
+        console.dir(request.vhost.host);
+        console.dir(request.vhost.hostname);
+        console.dir(request.vhost.length);
+        console.dir(request.vhost[0]);
+        response.send(request.vhost.host);
+    })
+);
+
+www.use(
+    vhost("pro.*", function handle(req, res, next) {
+        response.send(request.vhost.host);
+    })
+);
+
+www.use(vhost("server.*", server));
+
+www.get('/', (request, response) => {
+    response.send('yeet');
 });
-
-server.get('/Home.html', (request, response) => {
-    console.log('[i] Server got request for /...');
-    response.sendFile(path.join(__dirname + '/pages/Home.html'));
-});
-
-server.get('/Github.html', (request, response) => {
-    console.log('[i] Server got request for /Github.html...');
-    response.sendFile(path.join(__dirname + '/pages/Github.html'));
-});
-
-server.get('/MC%20Server.html', (request, response) => {
-    console.log('[i] Server got request for /...');
-    response.sendFile(path.join(__dirname + '/pages/MC Server.html'));
-});
-
-server.get('/MC%20Server/1432709913880345cda7289e67cfeaa1.jpg', (request, response) => {
-    console.log('[i] Server got request for /MC%20Server/1432709913880345cda7289e67cfeaa1.jpg...');
-    response.sendFile(path.join(__dirname + '/pages/MC Server/1432709913880345cda7289e67cfeaa1.jpg'));
-});
-
-server.get('/MC%20Server/c53a37ec0a1ffca0ff6027ab5e029bb4.jpg', (request, response) => {
-    console.log('[i] Server got request for /MC%20Server/c53a37ec0a1ffca0ff6027ab5e029bb4.jpg...');
-    response.sendFile(path.join(__dirname + '/pages/MC Server/c53a37ec0a1ffca0ff6027ab5e029bb4.jpg'))
-});
-
-server.get('/Home/0697e5697645bd89d6acbe8cb3f36c5c.jpg', (request, response) => {
-    console.log('[i] Server got request for /Home/0697e5697645bd89d6acbe8cb3f36c5c.jpg...');
-    response.sendFile(path.join(__dirname + '/pages/Home/0697e5697645bd89d6acbe8cb3f36c5c.jpg'))
-});
-
-server.get('/Home/a3f3c36821471f564d06f0c985666950.jpg', (request, response) => {
-    console.log('[i] Server got request for /Home/a3f3c36821471f564d06f0c985666950.jpg...');
-    response.sendFile(path.join(__dirname + '/pages/Home/a3f3c36821471f564d06f0c985666950.jpg'))
-});
-
-server.get('/Github/0d43241299e499aab27dc46f20f34256.jpg', (request, response) => {
-    console.log('[i] Server got request for /Github/0d43241299e499aab27dc46f20f34256.jpg...');
-    response.sendFile(path.join(__dirname + '/pages/Github/0d43241299e499aab27dc46f20f34256.jpg'))
-});
-
-server.get('/Github/d3c7bc0ae9d762f3d397fee2af89a3fa.jpg', (request, response) => {
-    console.log('[i] Server got request for /Github/d3c7bc0ae9d762f3d397fee2af89a3fa.jpg...');
-    response.sendFile(path.join(__dirname + '/pages/Github/d3c7bc0ae9d762f3d397fee2af89a3fa.jpg'))
-});
-
 
 // Downloads
 /*
